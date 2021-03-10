4c724e470df5631deb25eb2044b4e0c41051cf1c
jae-yong-2 <>
2021-03-10 17:51:06 +0900

    ML

---
+++ src/ML/Posting5.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import Img1 from './img/3-1.PNG'
+import Img2 from './img/3-2.PNG'
+class Posting3 extends Component{
+  render(){
+    return(
+      <div class="left">
+      <h1>Decision Tree-실습</h1>
+      <h4>
+      </h4>
+      </div>
+    );
+  }
+}
+
+export default Posting3
