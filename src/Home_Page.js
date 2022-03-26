15dcf104e11f67709e40c3434f2261f5b9f4fc1c
이재용(jae yong, lee) <49151220+jae-yong-2@users.noreply.github.com>
2022-03-26 18:51:32 +0900

    Update Home_Page.js

--- src/Home_Page.js
+++ src/Home_Page.js
@@ -19,7 +19,10 @@ class Home_Page extends Component{
             <br/>현재 개발자의 꿈을 가졌으며, 그것을 이루기 위해 조금씩 발전하는 모습을 이 블로그를 통해 담을 예정입니다.
             <br/>
             <br/>방슐랭가이드라는 스타트업 개발팀에서 프론트 팀장을 맡고 있으며 아래 링크에서 실제 제가 참여한 앱을 보실 수 있습니다.
-            <br/>
+            <a href="https://apps.apple.com/ng/app/%EB%B0%A9%EC%8A%90%EB%9E%AD%EA%B0%80%EC%9D%B4%EB%93%9C/id1594068890">
+                <button className="button_link">App_Store</button></a><br/>
+            <a href="https://play.google.com/store/search?q=%EB%B0%A9%EC%8A%90%EB%9E%AD%EA%B0%80%EC%9D%B4%EB%93%9C&c=apps">
+                <button className="button_link">Play_Store</button></a><br/>
             <input className='title_box' id='제목' placeholder="제목"/><br/>
             <div className='content_box' contentEditable="true" id='content' placeholder="content"></div>
               <button className='button_box' onClick={()=>{{
